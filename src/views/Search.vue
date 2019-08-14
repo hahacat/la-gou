fde1e33644ee497f076c0e3cac66a2efec873be6
unknown <568665691@qq.com>
2019-08-14 18:47:44 +0800

    second-day

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -5,37 +5,77 @@
         <span class="city">全国</span>
         <span class="cityicon"></span>
       </div>
+      <div class="rinputer">
+        <input type="text" placeholder="搜索职位或公司" class="inputer">
+        <span class="rbutton">
+          <em class="buttonicon"></em>
+        </span>
+      </div>
+    </div>
+    <div class="fulldialog">
+      <div class="fulldialog-content">
+        <h2 class="pop-title">热门城市</h2>
+        <table></table>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+
 export default {
   name: 'search'
 }
 </script>
 
 <style lang="scss" scoped>
+  $boder: 1px solid #e8e8e8;
   .linputer {
     display: flex;
-    border-bottom: 1px solid #e8e8e8;
+    border-bottom: $boder;
+    background: #ff0;
     .lbutton {
       width: 88px;
       height: 45px;
       line-height: 45px;
-      font-size: 0.14rem;
+      font-size: 0.16rem;
       display: flex;
       justify-content: center;
       align-items: center;
-      .city {
-        
-      }
+      border-right: $boder;
       .cityicon {
-        width: 8.5px;
-        height: 7px;
+        width: 15px;
+        height: 8px;
+        margin-left: 3px;
         background: {
           image: url('../assets/images/cityicon.png');
-          size:50% 100% ;
+          size: cover;
+        }
+      }
+    }
+    .rinputer {
+      flex: 1;
+      display: flex;
+      .inputer {
+        border: none;
+        flex: 1;
+        padding: 10px 0 10px 10px;
+        height: 25px;
+        line-height: 25px;
+        font-size: 0.16rem;
+        color: #333;
+      }
+      .rbutton {
+        width: 20%;
+        height: 45px;
+        border-left: $boder;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        .buttonicon {
+          width: 17px;
+          height: 17px;
+          background: url('../assets/images/search-active.png')
         }
       }
     }
